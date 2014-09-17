7c6c11618dfa8cff52528c49dcd1ad02fcf1c4a3
Brian Vermillion <BMVermillion@gmail.com>
2014-09-16 23:24:20 -0400

    Basic classes with unimplemented functions

---
+++ Behavior.java
@@ -0,0 +1,7 @@
+
+public interface Behavior {
+
+	void Idle(Nodes n);
+	void Master(Nodes n);
+	void Slave(Nodes n);
+}
---
+++ ToRun.java
@@ -0,0 +1,11 @@
+
+public class ToRun implements Behavior{
+
+	public void Idle(Nodes n) {
+	}
+	public void Master(Nodes n) {
+	}
+	public void Slave(Nodes n) {
+		
+	}
+}
