199ff902af1d0606a90a8f82d48bab34df3157a0
Brian Vermillion <BMVermillion@gmail.com>
2014-09-27 19:46:46 -0400

    Screen updates no longer event driven. A thread updates periodically

--- src/GUI.java
+++ src/GUI.java
@@ -5,27 +5,31 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 
-public class GUI implements ActionListener{
+public class GUI implements ActionListener, Runnable{
 	private JFrame window;
-;
-	private int dim = 500;
-	private int padding = dim / 10;
 	private Draw d;
+	private long time = 0;
 	
 	public GUI(Graph g) {
 	
 		
 		window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		d = new Draw(g);
+		
+		
 		
 		Container c = window.getContentPane();
 		c.setLayout(new BoxLayout(c,BoxLayout.PAGE_AXIS));
+		
+		//Creates new object to draw on
+		d = new Draw(g);
 		c.add(d);
 		
+		//Container for buttons
 		Container buttons = new Container();
 		buttons.setLayout(new BoxLayout(buttons, BoxLayout.LINE_AXIS));
 		
+		//Buttons with action listeners
 		JButton radius = new JButton("Radius");
 		radius.addActionListener(this);
 		JButton lines = new JButton("Lines");
@@ -38,6 +42,8 @@ public class GUI implements ActionListener{
 		connect.addActionListener(this);
 		JButton names = new JButton("Names");
 		names.addActionListener(this);
+		JButton stop = new JButton("Stop");
+		stop.addActionListener(this);		
 		
 		buttons.add(radius);
 		buttons.add(lines);
@@ -45,12 +51,13 @@ public class GUI implements ActionListener{
 		buttons.add(newn);
 		buttons.add(connect);
 		buttons.add(names);
+		buttons.add(stop);
 		
 		c.add(buttons);
 		
-		//window.setUndecorated(true);
-		window.setLocation(100,100);
 		
+		//Puts window on screen
+		window.setLocation(100,100);
 		window.pack();
 		window.setVisible(true);
 
@@ -71,73 +78,155 @@ public class GUI implements ActionListener{
 			d.drawConnections = !d.drawConnections;
 		else if (arg0.getActionCommand() == "Names")
 			d.drawNames = !d.drawNames;
+		else if (arg0.getActionCommand() == "Stop")
+			d.stop();
+		
+		d.stateChange = !d.stateChange;
+	}
+
 	
-		window.repaint();
+	
+	//Thread that handles the screen refresh rate
+	@Override
+	public void run() {
+		
+		time = System.currentTimeMillis();
+		
+		while (true) {
+			try {
+				Thread.sleep(15);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//if (d.stateChange) {
+				//System.out.println("Painting Window");
+				window.repaint();
+			//}
+			
+			long currentTime = System.currentTimeMillis();
+			d.fps = (int) (1000/(currentTime - time));
+			time = currentTime;
+			
+			
+		}
+		
 		
 	}
 
 
 }
 
+
+//New Class for drawing objects
 class Draw extends JPanel {
+	
+	//dimensions of the window
 	private int w = 550;
 	private int h = 550;
+	
+	//?
 	private int m = 25;
-	private Graph grr;
+	private Graph graph;
+	
+	//Values to control what is being displayed
 	public boolean drawRadius = false;
 	public boolean drawLines = true;
 	public boolean drawEdges = false;
 	public boolean drawConnections = true;
-	public boolean drawNames = false;
+	public boolean drawNames = true;
 	
+	//Saves system resources by letting the thread refreshing the 
+	//screen know that the display hasn't changed.
+	public boolean stateChange = false;
+	
+	//Current Frames Per Second of the display
+	public int fps = 0;
+	
+	//Draw constructor
 	public Draw(Graph g){
-		grr = g;
+		graph = g;
 		this.setPreferredSize(new Dimension(w,w));
 	}
 	
 	public void newGraph() {
-		grr = new Graph();
+		graph.stopThreads();
+		graph = new Graph();
+		graph.startThreads();
+		graph.all[10].getRunning().setMessageID(22);
 	}
 	
+	public void stop() {
+		graph.stopThreads();
+	}
+	
+	
+	//Function for instruction on how to paint the screen
 	public void paintComponent(Graphics g) {
 		
-		g.drawRect(m, m, w-m*2, h-m*2);
+		Graphics2D g2 = (Graphics2D)g;
+	    
+	    g2.drawString("FPS: "+fps, 1, 10);
+	    stateChange = !stateChange;
+		
+		g2.drawRect(m, m, w-m*2, h-m*2);
 		if (drawLines) 
 			for (int i=0; i<10; i++) {
-				g.drawLine(i*50 + m,0+m,i*50 + m,h-m);
-				g.drawLine(0+m,i*50+m,w-m,i*50+m);
+				g2.drawLine(i*50 + m,0+m,i*50 + m,h-m);
+				g2.drawLine(0+m,i*50+m,w-m,i*50+m);
 			}
 		
 		
 		if (drawEdges) {
-			g.setColor(Color.blue);
-			for (int i=0; i<grr.all.length; i++) {
-				Edge e = grr.all[i].getEdge();
+			g2.setColor(Color.blue);
+			for (int i=0; i<graph.all.length; i++) {
+				Edge e = graph.all[i].getEdge();
 				
 				if (e == null)
 					break;
 				
 				do {
 					//System.out.println("Edge");
-					g.drawLine((grr.all[i].getPos().x*50 + m), (grr.all[i].getPos().y*50 + m), (e.n.getPos().x*50 + m), (e.n.getPos().y*50 + m));
+					g2.drawLine((graph.all[i].getPos().x*50 + m), (graph.all[i].getPos().y*50 + m), (e.n.getPos().x*50 + m), (e.n.getPos().y*50 + m));
 					e = e.next;
 				}while (e != null);
 				//System.out.println("NextNode");
 			}
-			g.setColor(Color.black);
+			g2.setColor(Color.black);
 		}
 		
 		if (drawConnections) {
-			g.setColor(Color.GREEN);
-			for (int i=0; i<grr.all.length; i++) {
-				Edge e = grr.all[i].getEdge();
+			g2.setColor(Color.green);
+			for (int i=0; i<graph.all.length; i++) {
+				Edge e = graph.all[i].getEdge();
 				
 				if (e == null)
 					break;
 				
 				do {
-					if (!e.open)
-						g.drawLine((grr.all[i].getPos().x*50 + m), (grr.all[i].getPos().y*50 + m), (e.n.getPos().x*50 + m), (e.n.getPos().y*50 + m));
+					if (!e.open) {
+						int x1 = (graph.all[i].getPos().x*50 + m);
+						int x2 = (e.n.getPos().x*50 + m);
+						int y1 = (graph.all[i].getPos().y*50 + m);
+						int y2 = (e.n.getPos().y*50 + m);
+						int size = 3;
+						
+						if (Math.abs(y1-y2) >= Math.abs(x1-x2)) {
+							int px[] = {x2,x1-size,x1+size};
+							int py[] = {y2,y1,y1};
+							g2.fillPolygon(px,py, 3);
+							
+						}
+						else {
+							int px[] = {x2,x1,x1};
+							int py[] = {y2,y1-size,y1+size};
+							g2.fillPolygon(px,py, 3);
+						}
+						
+					
+					}
+						
 					e = e.next;
 				}while (e != null);
 
@@ -151,33 +240,33 @@ class Draw extends JPanel {
 		
 		//////////////////////////////Draws Nodes on the graph///////////////////////////////////
 		int radius = 16;
-		for (int i=0; i<grr.all.length; i++) {
+		for (int i=0; i<graph.all.length; i++) {
 			
 			
-			if (grr.all[i].getRel() == Relationship.Isolated)
-				g.setColor(Color.red);
-			else if (grr.all[i].getRel() == Relationship.Master )
-					g.setColor(Color.blue);
+			if (graph.all[i].getRel() == Relationship.Isolated)
+				g2.setColor(Color.red);
+			else if (graph.all[i].getRel() == Relationship.Master )
+					g2.setColor(Color.blue);
 			else 
-				g.setColor(Color.cyan);
+				g2.setColor(Color.cyan);
 			
-			g.fillOval( (grr.all[i].getPos().x*50 + m) - radius/2, (grr.all[i].getPos().y*50 + m) - radius/2, radius, radius);
+			g2.fillOval( (graph.all[i].getPos().x*50 + m) - radius/2, (graph.all[i].getPos().y*50 + m) - radius/2, radius, radius);
 			
 
 			if (drawNames) {
-				g.setColor(Color.GRAY);
-				g.drawString(grr.all[i].toString(), grr.all[i].getPos().x*50 + m - 10, grr.all[i].getPos().y*50 + m - 10);
+				g2.setColor(Color.GRAY);
+				g2.drawString(graph.all[i].toString(), graph.all[i].getPos().x*50 + m - 10, graph.all[i].getPos().y*50 + m - 10);
 			}
 		}
 		///////////////////////////////////////////////////////////////////////////////////////////
 		
 		
 		if (drawRadius) {
-			for (int i=0; i<grr.all.length; i++) {
-				g.setColor(Color.green);
+			for (int i=0; i<graph.all.length; i++) {
+				g2.setColor(Color.green);
 		
-				int tr = grr.all[i].getPos().rad*50*2;
-				g.drawOval((grr.all[i].getPos().x*50 + m) - tr/2, (grr.all[i].getPos().y*50 + m) - tr/2, tr, tr);
+				int tr = graph.all[i].getPos().rad*50*2;
+				g2.drawOval((graph.all[i].getPos().x*50 + m) - tr/2, (graph.all[i].getPos().y*50 + m) - tr/2, tr, tr);
 			}
 		}
 	}
